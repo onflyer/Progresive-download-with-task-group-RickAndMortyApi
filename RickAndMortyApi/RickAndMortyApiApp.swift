c21d4da4c2e9b544a4f8c11fc3d104e1b550a776
onflyer <salkaorama@gmail.com>
2023-07-10 12:54:14 +0200

    Initial Commit

---
+++ RickAndMortyApi/RickAndMortyApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApiApp.swift
+//  RickAndMortyApi
+//
+//  Created by Aleksandar Milidrag on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
